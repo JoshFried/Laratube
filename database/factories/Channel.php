ebd37bcd40dfdd81b5f0c0d4e648ed696627d9ee
JoshFried <josh.fried13@gmail.com>
2019-07-26 19:07:29 -0400

    created factories for testing purposes

---
+++ database/factories/Channel.php
@@ -0,0 +1,16 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use Laratube\Channel;
+use Faker\Generator as Faker;
+
+$factory->define(Channel::class, function (Faker $faker) {
+    return [
+        'name' => $faker->sentence(3), 
+        'user_id' => function() {
+            return factory(User::class)->create()->id;
+        }, 
+        'description' => $faker->sentence(30)
+    ];
+});
