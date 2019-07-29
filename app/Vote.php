c1100f749fcf5ca1860b8e48b78615685b2f1931
JoshFried <josh.fried13@gmail.com>
2019-07-29 19:20:59 -0400

    implemented vote component

--- app/Vote.php
+++ app/Vote.php
@@ -5,7 +5,7 @@ namespace Laratube;
 class Vote extends Model
 {
     // allow laravel to know other models can be polymporphic with vote
-    public function voteable () {
+    public function voteable() {;
         return $this->morphTo(); 
     }
 }
